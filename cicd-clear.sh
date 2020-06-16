f737f848323ffae188e87182449b062f055d1280
root <root@bastion.pk.test>
2020-06-16 14:26:56 +0200

    Modyfikacja konfiguracja.txt. Podmiana template w cicd-template.yaml na te z GitLab

--- cicd-clear.sh
+++ cicd-clear.sh
@@ -1,8 +1,11 @@
 # oc get rolebinding -n per-cicd
 
-oc delete all --all
-oc delete pvc gogs-data gogs-postgres-data nexus-pv sonardb sonarqube-data
+oc delete all --all -n per-cicd
+oc delete pvc gogs-data gogs-postgres-data nexus-pv sonardb sonarqube-data -n per-cicd
 oc delete rolebinding default_admin -n per-cicd
 oc delete cm gogs-config jenkins-slaves -n per-cicd
 oc delete sa gogs jenkins -n per-cicd
 oc delete secrets sonar-ldap-bind-dn sonardb -n per-cicd
+
+oc delete all --all -n per-dev
+oc delete all --all -n per-stage

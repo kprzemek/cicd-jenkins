8bf8389b5b334c084b4cf32544840c28d216c500
przemyslaw.kuznicki <root@bastion.pk.test>
2020-06-18 15:16:52 +0200

    Modyfikacja gitlab.txt

--- cicd-clear.sh
+++ cicd-clear.sh
@@ -5,7 +5,7 @@ oc delete pvc gogs-data gogs-postgres-data nexus-pv sonardb sonarqube-data -n pe
 oc delete rolebinding default_admin -n per-cicd
 oc delete cm gogs-config jenkins-slaves -n per-cicd
 oc delete sa gogs jenkins -n per-cicd
-oc delete secrets sonar-ldap-bind-dn sonardb -n per-cicd
+oc delete secrets sonar-ldap-bind-dn sonar-pgsql -n per-cicd
 
 oc delete all --all -n per-dev
 oc delete all --all -n per-stage

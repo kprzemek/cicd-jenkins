0fcf980836e76d74cdc706ded93267ffac886b00
przemyslaw.kuznicki <przemyslaw.kuznicki@linuxpolska.pl>
2021-03-15 12:52:17 +0100

    Modyfikacja cicd-template.yaml --as-deployment-config

--- cicd-clear.sh
+++ cicd-clear.sh
@@ -1,11 +1,11 @@
 # oc get rolebinding -n per-cicd
 
-oc delete all --all -n per-cicd
-oc delete pvc gogs-data gogs-postgres-data nexus-pv sonardb sonarqube-data -n per-cicd
-oc delete rolebinding default_admin -n per-cicd
-oc delete cm gogs-config jenkins-slaves -n per-cicd
-oc delete sa gogs jenkins -n per-cicd
-oc delete secrets sonar-ldap-bind-dn sonar-pgsql -n per-cicd
+oc delete all --all -n ep-cicd
+oc delete pvc gogs-data gogs-postgres-data nexus-pv sonardb sonarqube-data -n ep-cicd
+oc delete rolebinding default_admin -n ep-cicd
+oc delete cm gogs-config jenkins-slaves -n ep-cicd
+oc delete sa gogs jenkins -n ep-cicd
+oc delete secrets sonar-ldap-bind-dn sonar-pgsql -n ep-cicd
 
-oc delete all --all -n per-dev
-oc delete all --all -n per-stage
+oc delete all --all -n ep-dev
+oc delete all --all -n ep-stage
